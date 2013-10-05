19dbcaa87afc60710a9df0b4ebb298da9a618245
matt <matt@new-host-2.home>
2013-10-04 22:57:40 -0400

    Added simpler, non module pattern code

---
+++ trello.contextmenu.simple.js
@@ -0,0 +1,14 @@
+;(function($) {	
+  $(function() {
+    $('.list').not('.add-list').on('contextmenu', function(event) {
+      $(this).find('.js-open-list-menu').click();
+      event.preventDefault();
+    });
+
+    $('.list-card-details').on('contextmenu', function(event) {
+      $(this).find('.card-operation').click();
+      $(this).siblings('.card-operation').click();
+      return false;
+    });
+  });
+})(jQuery);
